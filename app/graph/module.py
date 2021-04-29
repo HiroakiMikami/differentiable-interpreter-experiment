335639565fbfae4cc32e2cc9872540cf40b2e392
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2021-04-29 17:05:44 +0900

    Add module for surrogate module

---
+++ app/graph/module.py
@@ -0,0 +1,64 @@
+import torch
+
+
+class Module(torch.nn.Module):
+    def __init__(
+        self,
+        C: int,
+        arities: list[int],
+        encoder: torch.nn.Module,
+        decoder: torch.nn.Module,
+    ):
+        super().__init__()
+        self.n_func = len(arities)
+        self.arities = arities
+        self.encoder = encoder
+        self.decoder = decoder
+
+        for i, arity in enumerate(arities):
+            if arity == 0:
+                p = torch.nn.Parameter(torch.zeros(C))
+                torch.nn.init.normal_(p)
+                self.register_parameter(f"func{i}", p)
+            else:
+                module = torch.nn.Sequential(
+                    torch.nn.Linear(arity * C, 4 * arity * C),
+                    torch.nn.Tanh(),
+                    torch.nn.Linear(4 * arity * C, 4 * C),
+                    torch.nn.Tanh(),
+                    torch.nn.Linear(4 * C, C)
+                )
+                self.add_module(f"func{i}", module)
+
+    def forward(self, p_func, args, p_args):
+        """
+        p_func: [N, n_func]
+        args: [N, n_arg, E]
+        p_args: [N, n_arity, n_arg]
+        """
+        N, n_func = p_func.shape
+        N, n_arity, n_arg = p_args.shape
+        args = self.encoder(args)  # [N, n_arg, C]
+        N, n_arg, C = args.shape
+        args = args.reshape(N, 1, n_arg, C)
+        p_args = p_args.reshape(N, n_arity, n_arg, 1)
+        args = p_args * args  # [N, n_arity, n_arg, C]
+        args = torch.sum(args, dim=2)  # [N, n_arity, C]
+
+        modules = dict(self.named_modules())
+        params = dict(self.named_parameters())
+        _out = []
+        for i, arity in enumerate(self.arities):
+            k = f"func{i}"
+            if arity == 0:
+                _out.append(params[k].reshape(1, -1).expand(N, -1))
+            else:
+                a = args[:, :arity, :]  # [N, arity, C]
+                a = a.reshape(N, -1)
+                _out.append(modules[k](a))
+        # _out: list of [N, C]
+        out = torch.stack(_out, dim=1)  # [N, n_func, C]
+        p_func = p_func.reshape(N, n_func, 1)
+        out = p_func * out  # [N, n_func, C]
+        out = torch.sum(out, dim=1)  # [N, C]
+        return self.decoder(out)
---
+++ test/graph/test_module.py
@@ -0,0 +1,35 @@
+import numpy as np
+import torch
+
+from app.graph.module import Module
+
+
+def test_module_shape():
+    module = Module(3, [1, 0], torch.nn.Linear(5, 3), torch.nn.Linear(3, 5))
+    out = module(
+        torch.rand(2, 2),
+        torch.rand(2, 7, 5),
+        torch.rand(2, 3, 7),
+    )
+    assert out.shape == (2, 5)
+
+
+def test_arity_mask():
+    module = Module(3, [1, 0], torch.nn.Linear(5, 3), torch.nn.Linear(3, 5))
+    p_func = torch.rand(2, 2)
+    args = torch.rand(2, 2, 5)
+    p_args = torch.rand(2, 3, 2)
+    with torch.no_grad():
+        out0 = module(p_func, args, p_args)
+        args[2:] = 0
+        out1 = module(p_func, args, p_args)
+    assert np.allclose(out0, out1)
+
+
+def test_empty_arg():
+    module = Module(3, [1, 0], torch.nn.Linear(5, 3), torch.nn.Linear(3, 5))
+    p_func = torch.rand(2, 2)
+    args = torch.rand(2, 0, 5)
+    p_args = torch.rand(2, 3, 0)
+    out = module(p_func, args, p_args)
+    assert out.shape == (2, 5)
