9be15b90dfca286b1cdfbe1644f3db7939f4f103
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2021-08-08 10:31:52 +0900

    Add simple infer test

--- test/test_train.py
+++ test/test_train.py
@@ -1,4 +1,5 @@
 from app import Interpreter, train, train_extractor
+from app.nn import normalize
 import tempfile
 import torch
 
@@ -82,3 +83,55 @@ def test_simple_train() -> None:
         z_out = interpreter.function_impl(z_add, [z_m1, z_1])
         out = interpreter.value_extractor.decode(z_out)[0, 0]
         assert torch.abs(out - 0) < 0.5, f"actual={out.item()}, expected=add(-1, 1)=0"
+
+
+
+def test_simple_infer() -> None:
+    # func[0] = id, func[1] = add
+    torch.manual_seed(0)
+    interpreter = Interpreter(8, 16, 2)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        train_extractor(
+            interpreter,
+            [lambda x: x[0], lambda x: x[0] + x[1]],
+            2,
+            1,
+            1000,
+            1e-3,
+            tmpdir,
+        )
+    with tempfile.TemporaryDirectory() as tmpdir:
+        train(
+            interpreter,
+            [lambda x: x[0], lambda x: x[0] + x[1]],
+            1,
+            1000,
+            1e-3,
+            tmpdir,
+        )
+    
+    z_f = normalize(torch.normal(mean=torch.zeros(1, 8), std=torch.ones(1, 8)))
+    z_c = normalize(torch.normal(mean=torch.zeros(1, 8), std=torch.ones(1, 8)))
+    z_f.requires_grad_(True)
+    z_c.requires_grad_(True)
+
+    optimizer = torch.optim.Adam([z_f, z_c], lr=1e-3)
+    # infer f(-2, x) => -3
+    with torch.no_grad():
+        z_m2 = interpreter.value_extractor.encode(torch.tensor([[-2.0]]))
+        out = torch.tensor([[-3.0]])
+    for _ in range(10000):
+        optimizer.zero_grad()
+        z_pred = interpreter.function_impl(z_f, [z_m2, z_c])
+        pred = interpreter.value_extractor.decode(z_pred)
+        loss = torch.nn.L1Loss()(pred, out)
+        loss.backward()
+        optimizer.step()
+    f = int(torch.round(interpreter.function_extractor.decode(z_f)))
+    c = int(torch.round(interpreter.value_extractor.decode(z_c)))
+
+    if f == 0:
+        out = -2
+    else:
+        out = -2 + c
+    assert out == -3
